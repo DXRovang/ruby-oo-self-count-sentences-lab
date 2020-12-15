31a2fa5a9d2750b759c7eea635df0567bc686d55
Alexander Rovang <wolf_vii@yahoo.com>
2020-12-14 19:17:47 -0500

    commit tree 3aedcdd42e7b7b414b69a975c6b65ee9ba6b2395

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    first = self.split(/[.?!]/)
+    first.map.with_index do |element, index|
+      if element == ""
+        first.delete_at(index)
+      end
+    end
+    y = first.length
+    y
   end
+  
 end
